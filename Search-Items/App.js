e6f5326c1f7170715dec6d25a362b400fec784ed
jaydeep apte <jaydeepresources@gmail.com>
2021-05-26 11:50:10 +0530

    Add files via upload

---
+++ Search-Items/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  newItem
+  searchItem
+
+  constructor() {
+    super()
+    this.state = {
+      items: ['Potato', 'Tomato', 'Pumpkin'],
+      defaultItems: ['Potato', 'Tomato', 'Pumpkin'],
+      buttonFlag: true
+    }
+  }
+
+  mapItems() {
+    const listItems = this.state.items.map((item, index) =>
+      <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+        <span>
+          {item}
+        </span>
+        <button className="btn btn-outline-danger material-icons btn-sm" onClick={() => this.deleteItem(index)}>clear</button>
+      </li>
+    )
+
+    return listItems
+  }
+
+  deleteItem(index) {
+    var temp = []
+    Object.assign(temp, this.state.items)
+
+    temp.splice(index, 1)
+    this.setState({
+      items: temp
+    })
+  }
+
+  addItem() {
+    var temp = []
+    Object.assign(temp, this.state.items)
+    temp.push(this.newItem.value)
+    this.setState({
+      items: temp
+    })
+    this.newItem.value = ''
+    this.setState({ buttonFlag: true })
+  }
+
+  textValueChanged() {
+    this.setState({
+      buttonFlag: (this.newItem.value.length < 1) ? true : false
+    })
+  }
+
+  searchValueChanged() {
+    if (this.searchItem.value.length === 0) {
+      this.setState({
+        items: this.state.defaultItems
+      })
+    }
+  }
+
+  search() {
+    var searchedItems = []
+    var tempItems = []
+    Object.assign(tempItems, this.state.items)
+
+    searchedItems = tempItems.filter(s => s.indexOf(this.searchItem.value) !== -1)
+    this.setState({
+      items: searchedItems
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <p className="display-4 my-3">Iterative Rendering</p>
+        <hr />
+
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" placeholder="enter item to be searched..."
+            ref={(input) => { this.searchItem = input }} onInput={() => this.searchValueChanged()} />
+          <button className="btn btn-outline-primary material-icons" onClick={() => this.search()}>search</button>
+        </div>
+
+        <ul className="list-group mb-2">
+          {this.mapItems()}
+        </ul>
+
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" placeholder="enter new item..." ref={(input) => { this.newItem = input }} onInput={() => this.textValueChanged()} />
+          <button className="btn btn-primary" type="button" onClick={() => this.addItem()} disabled={this.state.buttonFlag}>Add Item</button>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
